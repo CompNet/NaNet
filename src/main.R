074889291467aae6193ac9d26c7ced752995cb37
Vincent Labatut <vince.labatut@gmail.com>
2018-12-02 15:23:26 +0100

    project structure

---
+++ src/main.R
@@ -0,0 +1,20 @@
+# TODO: Add comment
+# 
+# Vincent Labatut
+# 11/2018
+###############################################################################
+
+
+
+# read the file describing the volumes
+
+
+# read the file describing the pages
+
+
+# read the file describing the interactions between characters
+
+
+
+
+
