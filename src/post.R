8b68f79d0c8b3ae048c36d7977ff90b8bdf1c659
Vincent Labatut <vince.labatut@gmail.com>
2021-06-28 14:00:37 +0200

    updated source code

--- src/post.R
+++ src/post.R
@@ -65,10 +65,50 @@ for(v in volumes)
 }
 
 # compare with ground truth
-threshold <- 5
+#threshold <- 5
+meas.names <- c("Precision", "Recall", "F-measure", "Spearman's rho", "Spearman's p-value")
+stats <- matrix(nrow=length(meas.names), ncol=length(volumes))
+colnames(stats) <- volumes
+rownames(stats) <- meas.names
 for(v in volumes)
 {	tlog(2, "Processing volume ",v)
-	ts <- min(which(freq.tab[,v]>0), threshold)
-	# TODO
+	#ts <- min(which(freq.tab[,v]>0), threshold)
 	
+	# get ground truth
+	gt <- freq.tab[freq.tab[,v]>0,v]
+	gt <- names(gt[order(gt,decreasing=TRUE)])
+	
+	# get estimation
+	#es <- est.tab[est.tab[,v]>0,v]
+	es <- est.tab[,v]
+	es <- names(es[order(es,decreasing=TRUE)])
+	
+	# compute Precision, Recall, F-measure for the top estimated characters
+	es1 <- es[es>0]
+	if(length(es)>=length(gt))
+		es1 <- es[1:length(gt)]
+	char.correct <- intersect(es1, gt)
+	char.surnum <- setdiff(es1, gt)
+	char.missing <- setdiff(gt, es1)
+	tlog(4, "Correct characters: ",paste(char.correct,collapse=", "))
+	tlog(4, "Superfluous characters: ",paste(char.surnum,collapse=", "))
+	tlog(4, "Missing characters: ",paste(char.missing,collapse=", "))
+	tp <- length(char.correct)
+	fp <- length(char.surnum)
+	fn <- length(char.missing)
+	pre <- tp/(tp+fp)
+	rec <- tp/(tp+fn)
+	fmeas <- 2*(pre*rec)/(pre+rec)
+	tlog(4, "Pre=",pre," Rec=",rec," F=",fmeas)
+	stats[c("Precision","Recall","F-measure"),v] <- c(pre,rec,fmeas)
+	
+	# compute spearman's correlation for the GT characters
+	lv <- sort(unique(union(es,gt)))
+	es2 <- intersect(es, gt)
+	print(cbind(gt,es2))
+	es2 <- as.integer(factor(es2, levels=lv))
+	gt2 <- as.integer(factor(gt, levels=lv))
+	rcor <- cor.test(x=gt2, y=es2, method="spearman")
+	tlog(4, "Spearman: cor=",rcor$estimate," p=",rcor$p.value)
+	stats[c("Spearman's rho", "Spearman's p-value"),v] <- c(rcor$estimate,rcor$p.value)
 }
