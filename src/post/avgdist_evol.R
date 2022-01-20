3f105ee5edd63a5af8c5f22420168cef681306d5
Vincent Labatut <vince.labatut@gmail.com>
2022-01-20 14:22:42 +0100

    completed post script (avg dist)

--- src/post/avgdist_evol.R
+++ src/post/avgdist_evol.R
@@ -16,24 +16,51 @@ data <- read.raw.data()
 # compute the sequence of scene-based graphs (possibly one for each scene)
 gs <- extract.static.graph.scenes(volume.info=data$volume.info, char.info=data$char.info, page.info=data$page.info, inter.df=data$inter.df, stats.scenes=data$stats.scenes, ret.seq=TRUE)
 
+# load full graph to get filtered characters
+graph.file <- get.path.graph.file(mode="scenes", ext=".graphml")
+g <- read_graph(file=graph.file, format="graphml")
+V(g)$name <- fix.encoding(strings=V(g)$name)
+V(g)$ShortName <- fix.encoding(strings=V(g)$ShortName)
+filt.names <- V(g)$name[V(g)$Filtered]
+
 # compute average distance for each graph in the sequence
 #print(any(sapply(gs, function(g) is_connected(g, mode="weak"))))	# check that each graph is connected
 g.orders <- future_sapply(gs, gorder)
 dist.vals <- future_sapply(gs, function(g) mean_distance(graph=g, directed=FALSE, unconnected=TRUE))
 
+# same for filtered graphs
+idx.rem <- which(gs[[1]]$Filtered)
+gs.filt <- future_sapply(gs, function(g) delete_vertices(g, v=idx.rem))
+g.orders.filt <- future_sapply(gs.filt, gorder)
+dist.vals.filt <- future_sapply(gs.filt, function(g) mean_distance(graph=g, directed=FALSE, unconnected=TRUE))
+
 # plot the distance as the graph order
 plot.file <- get.path.topomeas.plot(object="nodepairs", mode="scenes", meas.name="avgdist", plot.type="evolution_publication_lines")
+pal <- get.palette(2)
 for(fformat in PLOT_FORMAT)
 {	if(fformat==PLOT_FORMAT_PDF)
 		pdf(file=paste0(plot.file,PLOT_FORMAT_PDF), bg="white")
 	else if(fformat==PLOT_FORMAT_PNG)
 		png(filename=paste0(plot.file,PLOT_FORMAT_PNG), width=800, height=800, units="px", pointsize=20, bg="white")
-			# init plot
-			plot(x=g.orders, y=dist.vals, 
+			# init plot with unfiltered results
+			plot(
+				x=g.orders, y=dist.vals, 
 				xlab=TeX("Number of vertices $n$"),
 				ylab=TeX("Average distance $<d>$"),
-				col="RED",
+				col=pal[1],
 				type="l"
+			)
+			# plot filtered results
+			lines(
+				x=g.orders.filt, y=dist.vals.filt,
+				col=pal[2],
+				type="l"
+			)
+			# add legend
+			legend(
+				x="topright",
+				col=pal, #lty=1:2, 
+				legend=c("Unfiltered","Filtered")
 			)
 		dev.off()
 }
