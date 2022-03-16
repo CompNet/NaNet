a0923eaa1ee3334ca894014e59a63be7e56c8f6d
Vincent Labatut <vince.labatut@gmail.com>
2022-03-16 22:54:57 +0100

    finished char_tj_comp script (post)

--- src/post/description/char_tj_comp.R
+++ src/post/description/char_tj_comp.R
@@ -14,6 +14,9 @@ start.rec.log(text="CharComp")
 
 ################################################################################
 # main parameters
+filtered <- FALSE
+
+chars <- c("Thorgal Aegirsson","Jolan Thorgalsson")
 
 
 
@@ -26,80 +29,100 @@ tlog(2,"Reading previously computed corpus stats")
 data <- read.corpus.data()
 char.stats <- data$char.stats
 volume.stats <- data$volume.stats
+volume.chars <- data$volume.chars
 scene.stats <- data$scene.stats
 
 # monitored characters
-chars <- c("Thorgal Aegirsson","Jolan Thorgalsson")
-idx <- match(chars,char.stats[,COL_NAME])
+chars.short <- char.stats[match(chars,char.stats[,COL_NAME]),COL_NAME_SHORT]
 pal <- get.palette(5)[c(1,4)]
 
 # monitored volumes
-vols <- c(
-	"30", "31", "32",#"K1",
-	"33", #"L1", "K2",
-	"K3", #"L2", "J1", "L3",
-	"K4", "34", #"J2","L4",
-	"K5", #"L5", "J3", 
-	"K6", #"J4","L6",
-	"35"
-)
+#vols <- c(
+#	"28", "29",
+#	"30", "31", "32",#"K1",
+#	"33", #"L1", "K2",
+#	"K3", #"L2", "J1", "L3",
+#	"K4", "34", #"J2","L4",
+#	"K5", #"L5", "J3", 
+#	"K6", #"J4","L6",
+#	"35"
+#)
+vols <- volume.stats[,COL_VOLUME]
+vols <- vols[which(sapply(1:length(volume.chars), function(v) length(intersect(chars, volume.chars[[v]]))>0))]
+vols <- vols[-(1:11)]								# remove earlier volumes to save some space
+vols <- vols[-length(vols)]							# same with latter volumes
+vols <- vols[-which(grepl("J", vols, fixed=TRUE))]	# remove Young Thorgal volumes
 vols.idx <- match(vols, volume.stats[,COL_VOLUME])
 
 # read volume stats
 stats <- list() 
 for(v in 1:nrow(volume.stats))
-{	table.file <- get.path.stat.table(object="nodes", mode="scenes", weights="duration", vol=volume.stats[v,COL_VOLUME], filtered=FALSE)
+{	table.file <- get.path.stat.table(object="nodes", mode="scenes", weights="duration", vol=volume.stats[v,COL_VOLUME], filtered=filtered)
 	tt <- as.matrix(read.csv(table.file, header=TRUE, check.names=FALSE, row.names=1))
+	idx <- match(chars,rownames(tt))
 	if(v==1)
-	{	stats[[chars[1]]] <- tt[idx[1],]
-		stats[[chars[2]]] <- tt[idx[2],]
+	{	for(i in 1:length(chars))
+		{	if(!is.na(idx[i]))
+				stats[[chars[i]]] <- tt[idx[i],]
+			else
+				stats[[chars[i]]] <- rep(NA,ncol(tt))
+		}
 	}
 	else
-	{	stats[[chars[1]]] <- cbind(stats[[chars[1]]], tt[idx[1],])
-		stats[[chars[2]]] <- cbind(stats[[chars[2]]], tt[idx[2],])
+	{	for(i in 1:length(chars))
+		{	if(!is.na(idx[i]))
+				stats[[chars[i]]] <- cbind(stats[[chars[i]]], tt[idx[i],])
+			else
+				stats[[chars[i]]] <- cbind(stats[[chars[i]]], rep(NA,ncol(tt)))
+		}
 	}
 }
 
-# create the plot
-meass <- c(MEAS_DEGREE, MEAS_BETWEENNESS, MEAS_CLOSENESS, MEAS_EIGENCNTR)
-meas <- meass[1]
+# select measure
+meass <- c(MEAS_DEGREE, MEAS_BETWEENNESS, MEAS_CLOSENESS, MEAS_EIGENCNTR, MEAS_STRENGTH)
+meas <- meass[5]
+
+# get values
 vals <- c(stats[[1]][meas,vols.idx], stats[[2]][meas,vols.idx])
 vals <- vals[!is.na(vals)]
-# init empty plot
-plot(
-	NULL,
-	xlim=c(1,length(vols)), ylim=range(vals),
-	xlab="Volume", ylab=NODE_MEASURES[[meas]]$cname
-)
-# add mean value
-abline(h=mean(stats[[1]][meas,]), lty=2)
-# add lines
-for(i in 1:length(chars))
-{	lines(
-		x=1:length(vols), y=stats[[i]][meas,vols.idx],
-		col=pal[i]
-	)
+
+# create the plot
+plot.file <- get.path.topomeas.plot(object="graph", mode="scenes", meas.name=meas, filtered=filtered, plot.type=paste0("evol.by.volume_",paste0(chars.short,collapse="--")))
+tlog(6, "Plotting in file \"",plot.file,"\"")
+for(fformat in PLOT_FORMAT)
+{	if(fformat==PLOT_FORMAT_PDF)
+		pdf(file=paste0(plot.file,PLOT_FORMAT_PDF), bg="white", width=15, height=5)
+	else if(fformat==PLOT_FORMAT_PNG)
+		png(filename=paste0(plot.file,PLOT_FORMAT_PNG), width=2400, height=800, units="px", pointsize=20, bg="white")
+	par(mar=c(4,4,0,0)+0.1)	# remove the title space Bottom Left Top Right
+		# init empty plot
+		plot(
+			NULL,
+			xlim=c(1,length(vols)), ylim=range(vals),
+			xlab="Volume", ylab=NODE_MEASURES[[meas]]$cname,
+			xaxt="n"
+		)
+		axis(1,at=1:length(vols), labels=vols, cex.axis=0.75)
+		# add mean value
+		for(i in 1:length(chars))
+			abline(h=mean(stats[[i]][meas,]), lty=2, col=pal[i])
+		# add lines
+		for(i in 1:length(chars))
+		{	lines(
+				x=1:length(vols), y=stats[[i]][meas,vols.idx],
+				col=pal[i]
+			)
+		}
+		# legend
+		legend(
+			title="Characters",
+			x="topleft",
+			fill=pal,
+			legend=chars.short
+		)
+	# close file
+	dev.off()
 }
-# legend
-legend(
-	title="Characters",
-	x="topleft",
-	fill=pal,
-	legend=char.stats[idx,COL_NAME_SHORT]
-)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
