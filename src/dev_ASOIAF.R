5e9636b19890c97734bed877162b26463bf80570
Vincent Labatut <vince.labatut@gmail.com>
2023-02-18 22:26:59 +0100

    added new ASOIAF main script

---
+++ src/dev_ASOIAF.R
@@ -0,0 +1,70 @@
+# Main script to extract the networks, compute their topological measures, and 
+# generate the corresponding plots.
+# 
+# Vincent Labatut
+# 02/2023
+#
+# setwd("~/vlabatut/Eclipse/workspaces/Networks/NaNet")
+# setwd("C:/Users/Vincent/Eclipse/workspaces/Networks/NaNet")
+# source("src/dev_ASOIAF.R")
+###############################################################################
+
+
+
+
+###############################################################################
+# setup parameters
+SERIES <- "ASOIAF"
+
+
+
+
+###############################################################################
+# load scripts
+source("src/common/_include.R")
+
+# start logging
+start.rec.log(text=SERIES)
+
+
+
+
+###############################################################################
+###############################################################################
+# read raw data
+data.impl <- read.raw.data(char.det="implicit")
+# OR, if already computed, read from file
+#data.impl <- read.corpus.data(char.det="implicit")
+
+# compute corpus stats
+data.impl <- compute.corpus.stats(data=data.impl, char.det="implicit")
+
+# plot these stats
+plot.corpus.stats(data=data.impl, char.det="implicit")
+
+
+
+
+###############################################################################
+# extract scene-based networks
+data.impl <- extract.static.graphs.base(data=data.impl)
+
+# plot these graphs
+plot.static.graphs(data=data.impl)
+
+
+
+
+###############################################################################
+# compute scene-based graph stats
+compute.static.statistics.base(data=data.impl, char.det="implicit")
+
+# plot these stats
+generate.static.plots.base(data=data.impl)
+
+
+
+
+###############################################################################
+# stop logging
+end.rec.log()
