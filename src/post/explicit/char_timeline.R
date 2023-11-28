6a8115c6122a6eabf8a12b6dd65d7c76af1ea6d6
Vincent Labatut <vince.labatut@gmail.com>
2023-11-28 16:18:45 +0100

    adjusted timeline plot

--- src/post/explicit/char_timeline.R
+++ src/post/explicit/char_timeline.R
@@ -20,6 +20,7 @@ start.rec.log(text="CharTimeline")
 ###############################################################################
 # define targeted characters
 char.names <- c("Thorgal Aegirsson", "Aaricia Gandalfsdottir", "Kriss de Valnor", "Jolan Thorgalsson", "Louve Thorgalsdottir", "Muff")
+char.det <- "explicit"
 tlog(0,"Dealing with characters ",paste0(char.names,collapse=", "))
 
 
@@ -27,14 +28,14 @@ tlog(0,"Dealing with characters ",paste0(char.names,collapse=", "))
 
 ###############################################################################
 # read data
-tlog(0,"Reading data (explicit)")
-data.expl <- read.corpus.data(char.det="explicit")
-page.chars <- data.expl$page.chars
-scene.chars <- data.expl$scene.chars
-scene.stats <- data.expl$scene.stats
-volume.chars <- data.expl$volume.chars
-panel.stats <- data.expl$panel.stats
-panel.chars <- data.expl$panel.chars
+tlog(0,"Reading data (",char.det,")")
+data <- read.corpus.data(char.det=char.det)
+page.chars <- data$page.chars
+scene.chars <- data$scene.chars
+scene.stats <- data$scene.stats
+volume.chars <- data$volume.chars
+panel.stats <- data$panel.stats
+panel.chars <- data$panel.chars
 
 
 
@@ -42,10 +43,9 @@ panel.chars <- data.expl$panel.chars
 ###############################################################################
 tlog(0,"Complementing panel table")
 # complement panel table with approx scene id
-#sc.ids <- c()
-#for(p.id in 1:nrow(panel.stats))
-#	sc.ids <- c(sc.ids, which(scene.stats[,COL_PANEL_START_ID]>=p.id & scene.stats[,COL_PANEL_END_ID]<=p.id)[1])
-#panel.stats <- cbind(panel.stats, sc.ids)
+sc.ids <- get.approx.scenes.for.panels(scene.stats)
+panel.stats <- cbind(panel.stats, sc.ids)
+colnames(panel.stats)[ncol(panel.stats)] <- COL_SCENE_ID
 
 
 
@@ -53,22 +53,28 @@ tlog(0,"Complementing panel table")
 
 ###############################################################################
 tlog(0,"Looping over temporal units")
-for(tu in c("page","volume"))	# c("page","scene","volume")
+for(tu in c("page", "scene", "volume"))
 {	tlog(2,"Temporal unit: ",tu)
 	if(tu=="page")
 	{	tu.chars <- page.chars
 		tu.col <- COL_PAGE_ID
-		tu.xlab <- "Pages"
+		tu.xlab <- "Page"
+		key.param <- 20
+		tu.at <- seq(0, ncol(m), 100)
 	}
 	else if(tu=="scene")
 	{	tu.chars <- scene.chars
 		tu.col <- COL_SCENE_ID
-		tu.xlab <- "Scenes"
+		tu.xlab <- "Scene"
+		key.param <- 20
+		tu.at <- seq(0, ncol(m), 100)
 	}
 	else if(tu=="volume")
 	{	tu.chars <- volume.chars
 		tu.col <- COL_VOLUME_ID
-		tu.xlab <- "Volumes"
+		tu.xlab <- "Volume"
+		key.param <- 1
+		tu.at <- seq(0, ncol(m), 10)
 	}
 	
 	# count characters
@@ -84,12 +90,16 @@ for(tu in c("page","volume"))	# c("page","scene","volume")
 		}
 	}
 	
+	# setup breaks
+	breaks <- c(0, 1, seq(2,max(m),(max(m)-2)%/%4))
+	breaks <- c(breaks[-length(breaks)], max(m))
+		
 	# produce plot
-	plot.file <- get.path.stats.corpus(object="characters", char.det="explicit", subfold="timeline", pref=paste0("timeline_",tu,"s"))
+	plot.file <- get.path.stats.corpus(object="characters", char.det=char.det, subfold="timeline", pref=paste0("timeline_",tu,"s"))
 	pdf(paste0(plot.file,".pdf"), bg="white", width=15, height=3)
 		par(mar=c(5,7,4,5))		# plot margins: Bottom=5 Left=4 Top=4 Right=2
-		plot(m, border=NA, col=viridis, las=2, xlab=tu.xlab, ylab=NA, main="Timeline", cex.axis=0.7, spacing.key=c(20,20.5,0), axis.col=NULL)
-		axis(1, at=seq(0, ncol(m), 100), las=2)
+		plot(m, border=NA, col=viridis, breaks=breaks, las=2, xlab=tu.xlab, ylab=NA, main="Number of occurrences (panels)", cex.axis=0.7, spacing.key=c(key.param,key.param,0), axis.col=NULL)
+		axis(1, at=tu.at, las=2)
 	dev.off()
 }
 
